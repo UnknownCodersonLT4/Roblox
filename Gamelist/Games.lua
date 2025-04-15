0ff37d702c17f1a58d90787162397585f0465500
UnknownCodersonLT4 <coderobierika@gmail.com>
2025-04-15 00:54:38 -0400

    Create Games.lua

---
+++ Gamelist/Games.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [3623096087] = "https://raw.githubusercontent.com/UnknownCodersonLT4/Roblox/refs/heads/main/ML/Games.lua", -- Muscle Legends
+-- [3101667897] = -Los Coming soon
+}
+
+return Games
